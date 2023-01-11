04733f145e8c6f8535ce09986281b3d090a1fa3c
Manu <manuelchavarin@aori.mx>
2023-01-10 18:25:39 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AZ Equity Experts</title>
+</head>
+<body>
+    <h1>AZ Equity Experts</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ex totam porro earum dolore repellat veniam incidunt minus quidem error fugiat atque, sit inventore corporis quasi quia vel, dolorum quis.</p>
+</body>
+</html>
