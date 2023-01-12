ebb33debfd99e969af942fd1232e927f4027a8d7
Manu <manuelchavarin@aori.mx>
2023-01-12 11:53:28 -0600

    az avance

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
                 <button>
                     <img src="/img/small-logo.png" alt="">
                 </button>
-                <a class="main-button icon icon-right" href="#"><span>Agendar videollamada en Calendly</span><i class="fal fa-calendar-alt"></i></a>
+                <?php include('parts/call_to_action.html'); ?>
             </div>
         </nav>
         <div class="hero-text">
@@ -18,9 +18,39 @@
                 <div class="text">
                     <h1>Invierte en una propiedad en los EEUU</h1>
                     <p>Desde <span>150 MIL USD</span></p>
+                    <img class="house-mobile" src="/img/house-mobile.png" alt="">
+                    <?php include('parts/call_to_action.html'); ?>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="main-wrapper about-section">
+        <div class="gmw two">
+            <div class="img-wrapper">
+                <img src="/img/luis-mata.png" alt="">
+            </div>
+            <div class="info-about">
+                <h2>Acerca de nosotros</h2>
+                <h4>¡Hola! Soy Luis Mata</h4>
+                <span class="bullet">CEO y socio fundador</span>
+                <div class="item-circle">
+                    <p>Somos una firma inmobiliaria con 15 años de trayectoria, especializada en la compra-venta y administración de bienes raíces.</p>
+                </div>
+                <div class="item-circle end">
+                    <p>Nosotros somos el puente con inversionistas dentro y fuera de los EU que buscan comprar propiedades por debajo del valor de mercado sin necesidad de tener que administrarlos personalmente.</p>
+                </div>
+                <div class="icons-wrapper">
+                    <div class="icon-item">
+                        <div class="icon"><i class="fal fa-home"></i><p><span>15</span> años de experiencia</p></div>
+                        <div class="icon"><i class="fal fa-hands"></i><p><span>1324</span> tratos cerrados</p></div>
+                        <div class="icon"><i class="fal fa-list"></i><p><span>1324</span> propiedades listadas</p></div>
+                        <div class="icon"><i class="fal fa-face-smile-hearts"></i><p><span>4026</span> clientes satisfechos</p></div>
+                    </div>
+                </div>
+                <?php include('parts/call_to_action.html'); ?>
+            </div>
+        </div>
+
     </div>
 </body>
 </html>
